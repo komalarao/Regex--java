8061a26fe2e26864d27cf5b596583413e24930bf
komalarao <manoharvakamullu1999@gmail.com>
2022-11-30 09:58:06 -0800

    Refactor: enter phone number

--- src/main/java/com/bridgelabz/regexproblems/PhoneNumber.java
+++ src/main/java/com/bridgelabz/regexproblems/PhoneNumber.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.regexproblems;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-public class EmailId {
+public class PhoneNumber {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		// Method To Check First Name With Regex Start With Cap And 3 Min. Character
@@ -11,19 +11,29 @@ public class EmailId {
 		String lastName = scan.nextLine();
 		System.out.println("Enter The Mail : ");
 		String mail = scan.nextLine();
+		System.out.println("Enter The Mobile Number With County Code : ");
+		String mobileNumber = scan.nextLine();
+
 		if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
 			System.out.println("True");
 		} else {
 			System.out.println("False");
 		}
+
 		// Method To Check Last Name With Regex Start With Cap And 3 Min. Character
 		if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
 			System.out.println("True");
 		} else {
 			System.out.println("False");
 		}
+
 		// Method to check Valid Mail with Regex
 		if (Pattern.matches("^[a-zA-Z0-9]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", mail)) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+		if (Pattern.matches("^[+]91{1}\\s[0-9]{10}", mobileNumber)) {
 			System.out.println("True");
 		} else {
 			System.out.println("False");

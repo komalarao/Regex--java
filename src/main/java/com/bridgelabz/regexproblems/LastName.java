073ffa79e89e158b41af9395178eb0a06aae4bab
komalarao <manoharvakamullu1999@gmail.com>
2022-11-30 09:45:37 -0800

    add: valid last name

--- src/main/java/com/bridgelabz/regexproblems/LastName.java
+++ src/main/java/com/bridgelabz/regexproblems/LastName.java
@@ -1,12 +1,14 @@
 package com.bridgelabz.regexproblems;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-public class FirstName {
+
+public class LastName {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// Method To Check First Name With Regex Start With Cap And 3 Min. Character
 		System.out.println("Enter the first Name: ");
 		String firstName = scan.nextLine();
+		System.out.println("Enter the last Name :");
+		String lastName = scan.nextLine();
 
 		if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
 			System.out.println("First name is valid");
@@ -14,5 +16,10 @@ public class FirstName {
 			System.out.println("Entered first name is invalid");
 		}
 		scan.close();
+		if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
+			System.out.println("Last name is valid");
+		} else {
+			System.out.println("Entered last name is invalid");
+		}
 	}
 }
